d43e79582eef9e0a9103bb2eec2decdbab7d8288
primetoxinz <primetoxinz@gmail.com>
2014-08-06 15:10:16 -0400

    convert modlist to md

--- ModList_Create.py
+++ ModList_Create.py
@@ -3,7 +3,7 @@ import os
 currentDir = os.path.dirname(__file__)
 modsDir = currentDir + "/mods"
 
-with open('Modlist.txt', 'w') as mainfile:
+with open('Modlist.md', 'w') as mainfile:
 	for subdir, dirs, files in os.walk(dropboxModsDir):
 		for index, item in enumerate(files):
 			mainfile.write("* "+str(item).replace(".jar","").replace(".zip","").replace(".disabled", " [Disabled]").lower() + "\n")
