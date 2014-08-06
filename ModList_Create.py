f9d5a3792e801d0748f0e43bf94f7d0c58f50fe9
primetoxinz <primetoxinz@gmail.com>
2014-08-06 15:26:40 -0400

    fix again

--- ModList_Create.py
+++ ModList_Create.py
@@ -4,6 +4,6 @@ currentDir = os.path.dirname(__file__)
 modsDir = currentDir + "/mods"
 
 with open('Modlist.md', 'w') as mainfile:
-	for subdir, dirs, files in os.walk(dropboxModsDir):
+	for subdir, dirs, files in os.walk(modsDir):
 		for index, item in enumerate(files):
 			mainfile.write("* "+str(item).replace(".jar","").replace(".zip","").replace(".disabled", " [Disabled]").lower() + "\n")
